53e87337c9ae949a16ea885e5dda74ca910f7088
yukihane <yukihane.feather@gmail.com>
2015-11-08 19:29:05 +0900

    fixed by x68kace http://d.hatena.ne.jp/x68kace/20151021/1445353200

--- ox-hatena.el
+++ ox-hatena.el
@@ -50,7 +50,44 @@
     (table . org-hatena-table)
     (table-cell . org-hatena-table-cell)
     (table-row . org-hatena-table-row)
-    (template . org-hatena-template))
+    (template . org-hatena-template)
+    ;;; add ;;;
+    (bold . org-hatena-bold)
+    (italic . org-hatena-italic)
+    (underline . org-hatena-underline)
+    (code . org-hatena-code)
+    (verbatim . org-hatena-verbatim)
+    (strike-through . org-hatena-strike-through)
+    (latex-fragment . org-hatena-latex-fragment)
+    (timestamp . org-hatena-timestamp)
+    (center-block . org-hatena-center-block)
+    (clock . (lambda (&rest args1) (debug))) ; org-ascii-clock)
+    (drawer . org-hatena-drawer)
+    (dynamic-block . org-hatena-dynamic-block)
+    (entity . (lambda (&rest args2) (debug))) ; org-ascii-entity)
+    (export-block . (lambda (&rest args3) (debug))) ; org-ascii-export-block)
+    (export-snippet . (lambda (&rest args4) (debug))) ; org-ascii-export-snippet)
+    (horizontal-rule . org-hatena-horizontal-rule)
+    (inlinetask . (lambda (&rest args5) (debug))) ; org-ascii-inlinetask)
+    (latex-environment . (lambda (&rest args7) (debug))) ; org-ascii-latex-environment)
+    (planning . (lambda (&rest args8) (debug))) ; org-ascii-planning)
+    (quote-section . (lambda (&rest args9) (debug))) ;org-ascii-quote-section)
+    (radio-target . org-hatena-radio-target)
+    (special-block . org-hatena-special-block)
+    (statistics-cookie . (lambda (&rest args10) (debug))) ; org-ascii-statistics-cookie)
+    (subscript . org-hatena-subscript)
+    (superscript . org-hatena-superscript)
+    (verse-block . org-hatena-verse-block)
+    ;;;;;;;;;;
+    )
+  ;;; add ;;;
+  :export-block "hatena"
+  :options-alist
+  '((:title "TITLE" nil nil space)
+    (:filetags "FILETAGS" nil ":1:2:3:" nil)
+    (:date "DATE" nil nil t))
+  ;;;;;;;;;;;
+
   :menu-entry
   '(?b "Export to Hatena Notation"
        ((?B "As Hatena Notation buffer" org-hatena-export-as-hatena)
@@ -60,7 +97,7 @@
 
 ;;; Internal Variables
 
-(defvar org-hatena-extension "txt")
+(defvar org-hatena-extension "hat")
 
 (defvar org-hatena-output-mode 'diary)
 
@@ -89,11 +126,18 @@
 
 (defun org-hatena-item (item contents info)
   (let* ((type (org-element-property :type (org-export-get-parent item)))
-         (struct (org-element-property :structure item))
-         (bullet
-          (cond ((eq type 'unordered) "-")
-                ((eq type 'ordered) "+")))
-         text)
+   ;; (struct (org-element-property :structure (org-export-get-parent item)))
+   ;; add from ox-html
+   (tag (let ((tag (org-element-property :tag item)))
+    (and tag (org-export-data tag info))))
+   ;;
+   (bullet
+    (cond ((eq type 'unordered) "-")
+    ((eq type 'ordered) "+")
+    ;; descriptiveの時 ([-+].* :: .* )
+    ;; はてな記法では :～～:～～
+    ((eq type 'descriptive) (concat ":" tag ":"))))
+   text)
     (replace-regexp-in-string "^\\([^-+]\\)" (concat bullet "\\1")
                               (replace-regexp-in-string "^\\([-+]\\)" "\\1\\1" contents))))
 
@@ -191,7 +235,9 @@
 (defun org-hatena-export-to-hatena (&optional async subtreep visible-only body-only ext-plist)
   (interactive)
   (let* ((extension (concat "." org-hatena-extension))
-         (outfile (org-export-output-file-name extension subtreep)))
+   (outfile (org-export-output-file-name extension subtreep)) ;original
+;         (outfile (org-export-output-file-name extension subtreep org-hatena-output-directory))
+   )
     (if async
         (org-export-async-start
             (lambda (f) (org-export-add-to-stack f 'hatena))
@@ -205,6 +251,132 @@
   (if async (org-hatena-export-to-hatena t subtreep visible-only body-only)
     (org-open-file (org-hatena-export-to-hatena nil subtreep visible-only body-only))))
 
+;;; add ;;;
+;;;;; Bold
+(defun org-hatena-bold (bold contents info)
+  (format "<b>%s</b>" contents))
+;;;;; Italic
+(defun org-hatena-italic (italic contents info)
+  (format "<i>%s</i>" contents))
+;;;;; Underline
+(defun org-hatena-underline (underline contents info)
+  (format "<span style=\"text-decoration: underline;\">%s</span>" contents))
+;;;;; Code
+(defun org-hatena-code (code contents info)
+  "Return a CODE object from Org to Hatena.
+CONTENTS is nil.  INFO is a plist holding contextual
+information."
+  (format "<code>%s</code>" (org-element-property :value code)))
+;;;;; verbatim
+(defun org-hatena-verbatim (verbatim contents info)
+  "Return a VERBATIM object from Org to Hatena.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (format "<code>%s</code>"
+    (org-element-property :value verbatim)))
+
+;;;;; Strike-through
+(defun org-hatena-strike-through (strike-through contents info)
+  (format "<del>%s</del>" contents))
+
+; inport form ox-ascii
+(defun org-hatena-latex-fragment (latex-fragment contents info)
+  "Transcode a LATEX-FRAGMENT object from Org to ASCII.
+CONTENTS is nil.  INFO is a plist holding contextual
+information."
+  (when (plist-get info :with-latex)
+    (org-element-property :value latex-fragment)))
+
+(defun org-hatena-plain-text (text info)
+  "Transcode a TEXT string from Org to Hatena.
+TEXT is the string to transcode.  INFO is a plist holding
+contextual information."
+  (let ((output text))
+    ;; do nothing.
+    ;; Return value.
+    output))
+
+(defun org-hatena-timestamp (timestamp contents info)
+  "Transcode a TIMESTAMP object from Org to Hatena.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (org-hatena-plain-text (org-timestamp-translate timestamp) info))
+
+;;;; Center Block
+
+(defun org-hatena-center-block (center-block contents info)
+  "Transcode a CENTER-BLOCK element from Org to HATENA.
+CONTENTS holds the contents of the block.  INFO is a plist
+holding contextual information."
+  contents)
+
+;;;; Drawer
+
+(defun org-hatena-drawer (drawer contents info)
+  "Transcode a DRAWER element from Org to HATENA.
+CONTENTS holds the contents of the block.  INFO is a plist
+holding contextual information."
+  contents)
+
+
+;;;; Dynamic Block
+
+(defun org-hatena-dynamic-block (dynamic-block contents info)
+  "Transcode a DYNAMIC-BLOCK element from Org to HATENA.
+CONTENTS holds the contents of the block.  INFO is a plist
+holding contextual information."
+  contents)
+
+;;;; Horizontal Rule
+
+(defun org-hatena-horizontal-rule (horizontal-rule contents info)
+  "Transcode an HORIZONTAL-RULE  object from Org to HATENA.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  "<hr />")
+
+;;;; Radio Target
+
+(defun org-hatena-radio-target (radio-target contents info)
+  "Transcode a RADIO-TARGET object from Org to HATENA.
+CONTENTS is the contents of the target.  INFO is a plist holding
+contextual information."
+  (debug)
+  contents)
+
+;;;; Special Block
+
+(defun org-hatena-special-block (special-block contents info)
+  "Transcode a SPECIAL-BLOCK element from Org to HATENA.
+CONTENTS holds the contents of the block.  INFO is a plist
+holding contextual information."
+  contents)
+
+;;;; Subscript
+
+(defun org-hatena-subscript (subscript contents info)
+  "Transcode a SUBSCRIPT object from Org to HATENA.
+CONTENTS is the contents of the object.  INFO is a plist holding
+contextual information."
+  (if (org-element-property :use-brackets-p subscript)
+      (format "<sub>%s</sub>" contents)
+    (format "_%s" contents)))
+
+;;;; Superscript
+
+(defun org-hatena-superscript (superscript contents info)
+  "Transcode a SUPERSCRIPT object from Org to HATENA.
+CONTENTS is the contents of the object.  INFO is a plist holding
+contextual information."
+  (if (org-element-property :use-brackets-p superscript)
+      (format "<sup>%s</sup>" contents)
+    (format "^%s" contents)))
+
+;;;; Verse Block
+
+(defun org-hatena-verse-block (verse-block contents info)
+  "Transcode a VERSE-BLOCK element from Org to HATENA.
+CONTENTS is verse block contents.  INFO is a plist holding
+contextual information."
+  (format ">|\n%s|<" contents))
+;;;;;;;;;;;
 
 
 (provide 'ox-hatena)
